6f4232b4ee68f78f4d49e91b0ecdb6c361fa4aa3
ChidambaramPG <chidambarampg@gmail.com>
2022-05-14 02:33:17 +0530

    modified package name

--- src/android/AppStarter.java
+++ src/android/AppStarter.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
+
 import com.tonikorin.cordova.plugin.autostart.AutoStart;
 // import android.util.Log;
 
@@ -14,43 +16,46 @@ public class AppStarter {
     private static final String CORDOVA_AUTOSTART = "cordova_autostart";
 
     public void run(Context context, Intent intent, int componentState) {
-	     this.run(context, intent, componentState, false);
+        this.run(context, intent, componentState, false);
     }
 
     public void run(Context context, Intent intent, int componentState, boolean onAutostart) {
         // Enable or Disable UserPresentReceiver (or bypass the modification)
-        //Log.d("Cordova AppStarter", "UserPresentReceiver component, new state:" + String.valueOf(componentState));
-        if( componentState != BYPASS_USERPRESENT_MODIFICATION ) {
+        // Log.d("Cordova AppStarter", "UserPresentReceiver component, new state:" +
+        // String.valueOf(componentState));
+        if (componentState != BYPASS_USERPRESENT_MODIFICATION) {
             ComponentName receiver = new ComponentName(context, UserPresentReceiver.class);
             PackageManager pm = context.getPackageManager();
             pm.setComponentEnabledSetting(receiver, componentState, PackageManager.DONT_KILL_APP);
         }
 
         // Starting your app...
-        //Log.d("Cordova AppStarter", "STARTING APP...");
+        // Log.d("Cordova AppStarter", "STARTING APP...");
         SharedPreferences sp = context.getSharedPreferences(AutoStart.PREFS, Context.MODE_PRIVATE);
         String packageName = context.getPackageName();
         String activityClassName = sp.getString(AutoStart.ACTIVITY_CLASS_NAME, "");
-        if( !activityClassName.equals("") ){
-            //Log.d("Cordova AppStarter", className);
+        if (!activityClassName.equals("")) {
+            // Log.d("Cordova AppStarter", className);
+            Log.println(Log.VERBOSE, "Xogo", "Starting activity intent");
             Intent activityIntent = new Intent();
             activityIntent.setClassName(
-                context, String.format("%s.%s", packageName, activityClassName));
+                    context, String.format("%s.%s", packageName, activityClassName));
             activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             if (onAutostart) {
-              activityIntent.putExtra(CORDOVA_AUTOSTART, true);
+                // activityIntent.putExtra(CORDOVA_AUTOSTART, true);
             }
             context.startActivity(activityIntent);
         }
-        // Start a service in the background.
+        // Start a service in th
         String serviceClassName = sp.getString(AutoStart.SERVICE_CLASS_NAME, "");
-        String servicePackageName = serviceClassName.substring(0, serviceClassName.lastIndexOf("."));
-        if ( !serviceClassName.equals("") ) {
+        String servicePackageName = "xogo.xogoplayer";
+        if (!serviceClassName.equals("")) {
+            Log.println(Log.VERBOSE, "Xogo", "Starting service intent");
             Intent serviceIntent = new Intent();
             serviceIntent.setClassName(servicePackageName, serviceClassName);
-            if ( onAutostart ) {
-                serviceIntent.putExtra(CORDOVA_AUTOSTART, true);
+            if (onAutostart) {
+                // serviceIntent.putExtra(CORDOVA_AUTOSTART, true);
             }
             context.startService(serviceIntent);
         }
